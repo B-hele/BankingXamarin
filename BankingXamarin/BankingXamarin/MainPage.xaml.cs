ff43f0b90647203e6e80de0b47829366f763cff3
B-hele <70335488+B-hele@users.noreply.github.com>
2020-11-12 15:36:23 +0200

    WE MOVING

--- BankingXamarin/BankingXamarin/MainPage.xaml.cs
+++ BankingXamarin/BankingXamarin/MainPage.xaml.cs
@@ -5,14 +5,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Banking;
 
 namespace BankingXamarin
 {
     public partial class MainPage : ContentPage
     {
+        private BankAccount _account;
         public MainPage()
         {
             InitializeComponent();
+
+            Bank fnb = new Bank("First National Bank", 4324, "Kenilworth");
+            Customer myNewCustomer = new Customer("7766445424", "10 me at home", "Bob", "The Builder");
+            fnb.AddCustomer(myNewCustomer);
+
+            var account = myNewCustomer.ApplyForBankAccount();
+        }
+
+        private void DepositButton_Clicked(object sender, EventArgs e)
+        {
+            decimal depositAmount = 0;
+            var valid = decimal.TryParse(DepositAmountEntry.Text, out depositAmount);
+            var reason = DepositReasonEntry.Text;
+            if (valid)
+            {
+                _account.DepositMoney(depositAmount, DateTime.Now, reason);
+            }
+            else
+            {
+                DisplayAlert("Validation Error", "Please Enter a Number", "Cancel");
+            }
+
         }
     }
 }
